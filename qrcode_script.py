762b097134f28d51fb0e9686f664067c3ba49be2
SkyVandal <bruno.codinha@tecnico.ulisboa.pt>
2022-07-06 13:51:14 +0100

    menu2

--- qrcode_script.py
+++ qrcode_script.py
@@ -1,4 +1,7 @@
 import qrcode
+from PIL import Image
+import PIL
+
 img = qrcode.make("https://www.qrcodi.com/7iks/pages/menu2.html")
 type(img)  # qrcode.image.pil.PilImage
 img.save("_qrcode.png")
